cd286874317992513193ebe21234990d856a6a59
saiteja4634 <saitejcomfortk2@gmail.com>
2022-06-11 22:59:46 +0530

    first project changes

--- main.py
+++ main.py
@@ -37,18 +37,35 @@ def get_incomes():
     query_job = bigquery_client.query(
         """
         SELECT 
-          reactions
+          TABLE_UUID
         FROM 
-          `bigquery-public-data.fda_food.food_events` LIMIT 100
+          `hsbc-9093058-rwapc51-dev.CATALOGUE.VM_METADATA` CROSS JOIN UNNEST(METADATA) AS A
+           WHERE FILE_TYPE like 'fotc_rd_cost_centre_to_cg%' 
+           AND (A.ATTRIBUTE = 'location' AND  A.VALUE = 'GBQ')
+           ORDER BY CREATED DESC LIMIT 1
         """
     )
 
     # Handle query_job result and return to flask to display
     res = query_job.result()
+    tableName = ""
+    for row in res:
+        tableName = str(row)
+
+    finalTableName = "hsbc-9093058-rwapc51-dev." + tableName
+    sqlQuery = """
+    select * from `{0}`
+    """.format(finalTableName)
+    query_job = bigquery_client.query(sqlQuery)
+    finalResponse = query_job.result()
     data = []
     for row in res:
         dict = {}
-        dict["reactions"] = str(row[0])
+        dict["__uuid"] = str(row[0])
+        dict["Cost_Centre"] = str(row[1])
+        dict["Customer_Group"] = str(row[2])
+        dict["Customer_subgroup_Level_1"] = str(row[3])
+        dict["Customer_subgroup_Level_2"] = str(row[4])
         data.append(dict)
     finalData = json.dumps(data)
     pandas.read_json(finalData).to_excel("output.xlsx")
